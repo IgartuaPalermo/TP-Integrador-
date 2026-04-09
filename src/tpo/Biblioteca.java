b60466e88d4dbe352d54c73addea49517d875141
Fillozo <morkzato@gmail.com>
2026-04-09 19:52:43 -0300

    Metiendo caracteristicas basicas

--- src/tpo/Biblioteca.java
+++ src/tpo/Biblioteca.java
@@ -2,4 +2,7 @@ package tpo;
 
 public class Biblioteca {
 
+    public cargarLibro(int isbn, String titulo, String autor, int copias){
+        tpo.Libro libro = new tpo.Libro(isbn, titulo, autor, copias);
+    }
 }
