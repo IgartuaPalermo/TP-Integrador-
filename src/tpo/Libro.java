0a4497af3ec2e927f26a322884267bf40aa01046
Fillozo <morkzato@gmail.com>
2026-04-08 22:11:13 -0300

    Creando Objeto Libro, era esto lo que querias?

--- src/tpo/Libro.java
+++ src/tpo/Libro.java
@@ -1,8 +1,57 @@
 package tpo;
 
 public class Libro {
-    int isbn;
-    String titulo;
-    String autor;
-    int copiasDisponibles;
+    private int isbn;
+    private String titulo;
+    private String autor;
+    private int copiasDisponibles;
+    private ColaPU listaEspera = new ColaPU();
+
+    public void Libro(int isbn, String titulo, String autor, int copiasDisponibles){
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.copiasDisponibles = copiasDisponibles;
+        listaEspera.InicializarCola();
+    }
+
+    public void AgregarEspera(int dni){
+        listaEspera.Acolar(dni);
+    }
+
+    public void QuitarEspera(){
+        listaEspera.Desacolar();
+    }
+    //Podriamos combinar las funciones quitarespera y primeroespera?
+    public int PrimeroEnEspera(){
+        return listaEspera.Primero();
+    }
+
+    public boolean CheckNoHayEspera(){
+        return listaEspera.ColaVacia();
+    }
+
+    public ColaPU GetEspera() {
+        return listaEspera;
+    }
+
+    public String GetTitulo(){
+        return titulo;
+    }
+
+    public int GetIsbn(){
+        return isbn;
+    }
+
+    public int GetCopias(){
+        return copiasDisponibles;
+    }
+
+    public void AumentarCopias(){
+        copiasDisponibles++;
+    }
+
+    public void DisminuirCopias(){
+        copiasDisponibles--;
+    }
 }
