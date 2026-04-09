4a5f956583f4cbc4f8bebd483bebe2c44d093da2
Fillozo <morkzato@gmail.com>
2026-04-08 22:11:53 -0300

    No se muy bien que hay que meterle a esta clase, asi que mientras le añadi un constructor.

--- src/tpo/Usuario.java
+++ src/tpo/Usuario.java
@@ -4,4 +4,9 @@ public class Usuario {
     int dni;
     String nombre;
     // ConjuntoTDA prestamos;
+
+    public void Usuario(int dni, String nombre){
+        this.dni = dni;
+        this.nombre = nombre;
+    }
 }
