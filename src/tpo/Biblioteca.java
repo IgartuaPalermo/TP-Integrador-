1a110170476f4f2880c00cbb552c4ecf27661798
Fillozo <morkzato@gmail.com>
2026-04-09 19:52:45 -0300

    Metiendo caracteristicas basicas

--- src/tpo/Biblioteca.java
+++ src/tpo/Biblioteca.java
@@ -1,6 +1,9 @@
 package tpo;
 
 public class Biblioteca {
+    private DiccionarioSimpleTDA libros;
+    private DiccionarioSimpleTDA usuarios;
+    private ConjuntoTDA librosDisponibles;
 
     public cargarLibro(int isbn, String titulo, String autor, int copias){
         tpo.Libro libro = new tpo.Libro(isbn, titulo, autor, copias);
